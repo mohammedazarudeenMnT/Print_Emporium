3143ba2dd99635691375f72421111ca2f623756a
mohammedazarudeenMnT <azar@mntfuture.com>
2026-01-29 17:10:47 +0530

    "removed unwanted auth routes"

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,20 +1,8 @@
 import express from "express";
-import {
-  register,
-  login,
-  logout,
-  getSession,
-  forgetPassword,
-  resetPassword,
-} from "../controllers/auth.controller.js";
+import { resetPassword } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.post("/register", register);
-router.post("/login", login);
-router.post("/logout", logout);
-router.get("/session", getSession);
-router.post("/forget-password", forgetPassword);
 router.post("/reset-password", resetPassword);
 
 export default router;
